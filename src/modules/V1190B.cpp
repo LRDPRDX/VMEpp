09b7c70bab5cf40d517fa6b8931f0910618b6db7
gergler <sayfudinovap@gmail.com>
2021-03-05 11:41:14 +0700

    Override functions, create V1190B.cpp

---
+++ src/modules/V1190B.cpp
@@ -0,0 +1,36 @@
+#include "modules/V1190B.h"
+
+
+
+namespace vmeplus {
+    void V1190B::Initialize() {}
+
+    void V1190B::Release() {}
+
+    void V1190B::AllocateBuffer() {}
+
+    uint32_t V1190B::ReadBuffer() {return 0;}
+
+    bool V1190B::GetEvent(VEvent *event) {return 0;}
+
+    bool V1190B::GetEventAt(uint32_t index, VEvent *event) const {return 0;}
+
+    void V1190B::WriteIRQLevel(uint16_t level) {}
+
+    uint16_t V1190B::ReadIRQLevel() { return 0; }
+
+    void V1190B::WriteIRQVector(uint16_t vector) {}
+
+    uint16_t V1190B::ReadIRQVector() { return 0; }
+
+    void V1190B::ISR(uint16_t vector) {}
+
+    void V1190B::Reset() {}
+
+    void WriteRegister(uint32_t data) {}
+
+    uint32_t ReadRegister() { return 0;}
+
+    bool WriteOpcode(uint16_t opcode) { return 0; }
+    bool ReadOpcode(uint16_t opcode) { return 0; }
+}
