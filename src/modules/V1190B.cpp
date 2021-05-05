7d5b48bdc76f7fc14f46e1aa8d3e7c53061b691a
gergler <sayfudinovap@gmail.com>
2021-05-05 17:40:57 +0700

    add NLI

--- src/modules/V1190B.cpp
+++ src/modules/V1190B.cpp
@@ -7,12 +7,11 @@ namespace vmeplus {
             VSlaveInterrupter("V1190B", baseAddress, range),
             VSlaveAcquisitor("V1190B", baseAddress, range),
 
-            fFirmwareRevision( "N/A" ),
-            fOUI( 0 ),
-            fVersion( 0 ),
-            fBoardID( 0 ),
-            fRevision( 0 )
-    {}
+            fFirmwareRevision("N/A"),
+            fOUI(0),
+            fVersion(0),
+            fBoardID(0),
+            fRevision(0) {}
 
     V1190B::~V1190B() {};
 
@@ -20,7 +19,7 @@ namespace vmeplus {
         PrintMessage(Message_t::INFO, "Inititalizing " + fName + "...");
 
         //READ ROM
-        PrintMessage( Message_t::INFO, "Reading ROM of " + fName + "..." );
+        PrintMessage(Message_t::INFO, "Reading ROM of " + fName + "...");
         ReadSerialNumber();
         ReadFirmRevNumber();
         ReadOUI();
@@ -34,29 +33,24 @@ namespace vmeplus {
     void V1190B::Release() {}
 
     /****** DATA ACQUISITION ******/
-    void V1190B::WriteEventsBLT( uint8_t n )
-    {
-        WriteRegister16( V1190B_BLT_EVENT_NUMBER, n, 0x00FF );
+    void V1190B::WriteEventsBLT(uint8_t n) {
+        WriteRegister16(V1190B_BLT_EVENT_NUMBER, n, 0x00FF);
     }
 
-    uint8_t V1190B::ReadEventsBLT()
-    {
-        return ReadRegister16( V1190B_BLT_EVENT_NUMBER, 0x00FF );
+    uint8_t V1190B::ReadEventsBLT() {
+        return ReadRegister16(V1190B_BLT_EVENT_NUMBER, 0x00FF);
     }
 
-    uint32_t V1190B::ReadEventCounter()
-    {
-        return ReadRegister32( V1190B_EVENT_COUNTER );
+    uint32_t V1190B::ReadEventCounter() {
+        return ReadRegister32(V1190B_EVENT_COUNTER);
     }
 
-    uint16_t V1190B::ReadEventsStored()
-    {
-        return ReadRegister16( V1190B_EVENT_STORED );
+    uint16_t V1190B::ReadEventsStored() {
+        return ReadRegister16(V1190B_EVENT_STORED);
     }
 
-    void V1190B::SendSWTrigger()
-    {
-        WriteRegister16( V1190B_SOFTWARE_TRIGGER, 1 );
+    void V1190B::SendSWTrigger() {
+        WriteRegister16(V1190B_SOFTWARE_TRIGGER, 1);
     }
 
     void V1190B::AllocateBuffer() {}
@@ -68,14 +62,12 @@ namespace vmeplus {
     bool V1190B::GetEventAt(uint32_t index, VEvent *event) const { return 0; }
 
     /****** INTERRUPTER ******/
-    void V1190B::WriteIRQEvents( uint16_t n )
-    {
-        WriteRegister16( V1190B_ALMOST_FULL_LEVEL, (n > 0) ? n : 1 );
+    void V1190B::WriteIRQEvents(uint16_t n) {
+        WriteRegister16(V1190B_ALMOST_FULL_LEVEL, (n > 0) ? n : 1);
     }
 
-    uint16_t V1190B::ReadIRQEvents()
-    {
-        return ReadRegister16( V1190B_ALMOST_FULL_LEVEL );
+    uint16_t V1190B::ReadIRQEvents() {
+        return ReadRegister16(V1190B_ALMOST_FULL_LEVEL);
     }
 
     void V1190B::WriteIRQLevel(uint16_t level) {
@@ -97,20 +89,17 @@ namespace vmeplus {
     void V1190B::ISR(uint16_t vector) {}
 
     /****** RESET ******/
-    void V1190B::Reset()
-    {
+    void V1190B::Reset() {
         WriteSoftwareClear();
         WriteSingleShotReset();
     }
 
-    void V1190B::WriteSoftwareClear()
-    {
-        WriteRegister16( V1190B_SOFTWARE_CLEAR, 1 );
+    void V1190B::WriteSoftwareClear() {
+        WriteRegister16(V1190B_SOFTWARE_CLEAR, 1);
     }
 
-    void V1190B::WriteSingleShotReset()
-    {
-        WriteRegister16( V1190B_MODULE_RESET, 1 );
+    void V1190B::WriteSingleShotReset() {
+        WriteRegister16(V1190B_MODULE_RESET, 1);
     }
 
     void V1190B::WriteMicro(uint16_t data) {
@@ -295,7 +284,7 @@ namespace vmeplus {
     const std::array<uint8_t, 33> V1190B::fEEPROM = {0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09,
                                                      0x0A, 0x0B, 0x0C, 0x0D, 0x0E, 0x0F, 0x10, 0x11,
                                                      0x18, 0x19, 0x1A, 0x1B, 0x1C, 0x1D, 0x1E, 0x1F,
-                                                     0x20, 0x21, 0x22,0x23, 0x24, 0x25, 0x26, 0x27, 0x28};
+                                                     0x20, 0x21, 0x22, 0x23, 0x24, 0x25, 0x26, 0x27, 0x28};
 
     void V1190B::WriteEEPROM(uint16_t address, uint16_t data) {
         if (std::find(fEEPROM.begin(), fEEPROM.end(), address) == fEEPROM.end()) {
@@ -354,45 +343,39 @@ namespace vmeplus {
     }
 
     //CHANNEL
-    void V1190B::WriteEnableChannel( uint8_t n, bool status )
-    {
-        if( status ) WriteMicro( Opcode( Command::EN_CHANNEL, n % fChNumber ) );
-        else         WriteMicro( Opcode( Command::DIS_CHANNEL, n % fChNumber ) );
+    void V1190B::WriteEnableChannel(uint8_t n, bool status) {
+        if (status) WriteMicro(Opcode(Command::EN_CHANNEL, n % fChNumber));
+        else WriteMicro(Opcode(Command::DIS_CHANNEL, n % fChNumber));
     }
 
-    void V1190B::WriteEnableAll( bool status )
-    {
-        if( status ) WriteMicro( Opcode( Command::EN_ALL_CH ) );
-        else         WriteMicro( Opcode( Command::DIS_ALL_CH ) );
+    void V1190B::WriteEnableAll(bool status) {
+        if (status) WriteMicro(Opcode(Command::EN_ALL_CH));
+        else WriteMicro(Opcode(Command::DIS_ALL_CH));
     }
 
 
-    void V1190B::WriteEnablePattern( V1190B::TDC tdc, uint32_t pattern )
-    {
-        switch( tdc )
-        {
-            case( TDC::TDC0 ) :
-                WriteMicro( Opcode( Command::WRITE_EN_PATTERN32, 0x0000 ) );
+    void V1190B::WriteEnablePattern(V1190B::TDC tdc, uint32_t pattern) {
+        switch (tdc) {
+            case (TDC::TDC0) :
+                WriteMicro(Opcode(Command::WRITE_EN_PATTERN32, 0x0000));
                 break;
-            case( TDC::TDC1 ) :
-                WriteMicro( Opcode( Command::WRITE_EN_PATTERN32, 0x0001 ) );
+            case (TDC::TDC1) :
+                WriteMicro(Opcode(Command::WRITE_EN_PATTERN32, 0x0001));
                 break;
         }
-        uint16_t lsb = (pattern & 0x0000ffff ); // ch 0 : 15
+        uint16_t lsb = (pattern & 0x0000ffff); // ch 0 : 15
         uint16_t msb = (pattern >> 16U);        // ch 16 : 31
-        WriteMicro( lsb );
-        WriteMicro( msb );
+        WriteMicro(lsb);
+        WriteMicro(msb);
     }
 
-    uint32_t V1190B::ReadEnablePattern( V1190B::TDC tdc )
-    {
-        switch( tdc )
-        {
-            case( TDC::TDC0 ) :
-                WriteMicro( Opcode( Command::READ_EN_PATTERN32, 0x0000 ) );
+    uint32_t V1190B::ReadEnablePattern(V1190B::TDC tdc) {
+        switch (tdc) {
+            case (TDC::TDC0) :
+                WriteMicro(Opcode(Command::READ_EN_PATTERN32, 0x0000));
                 break;
-            case( TDC::TDC1 ) :
-                WriteMicro( Opcode( Command::READ_EN_PATTERN32, 0x0001 ) );
+            case (TDC::TDC1) :
+                WriteMicro(Opcode(Command::READ_EN_PATTERN32, 0x0001));
                 break;
         }
         uint32_t lsb = ReadMicro(); // ch 0 : 15
@@ -402,86 +385,73 @@ namespace vmeplus {
     }
 
     // TDC Readout
-    void V1190B::WriteEnableHeaderTrailer( bool status )
-    {
-        if( status ) WriteMicro( Opcode( Command::EN_HEAD_TRAILER ) );
-        else         WriteMicro( Opcode( Command::DIS_HEAD_TRAILER ) );
+    void V1190B::WriteEnableHeaderTrailer(bool status) {
+        if (status) WriteMicro(Opcode(Command::EN_HEAD_TRAILER));
+        else WriteMicro(Opcode(Command::DIS_HEAD_TRAILER));
     }
 
-    bool V1190B::ReadEnableHeaderTrailer()
-    {
-        WriteMicro( Opcode( Command::READ_HEAD_TRAILER ) );
+    bool V1190B::ReadEnableHeaderTrailer() {
+        WriteMicro(Opcode(Command::READ_HEAD_TRAILER));
         uint16_t data = ReadMicro();
         return (1U & data);
     }
 
-    void V1190B::WriteMaxHitsPerEvent( V1190B::MaxHitsPerEvent n )
-    {
-        WriteMicro( Opcode( Command::SET_EVENT_SIZE ) );
-        WriteMicro( static_cast<uint16_t>( n ) );
+    void V1190B::WriteMaxHitsPerEvent(V1190B::MaxHitsPerEvent n) {
+        WriteMicro(Opcode(Command::SET_EVENT_SIZE));
+        WriteMicro(static_cast<uint16_t>( n ));
     }
 
-    V1190B::MaxHitsPerEvent V1190B::ReadMaxHitsPerEvent()
-    {
-        WriteMicro( Opcode( Command::READ_EVENT_SIZE ) );
+    V1190B::MaxHitsPerEvent V1190B::ReadMaxHitsPerEvent() {
+        WriteMicro(Opcode(Command::READ_EVENT_SIZE));
         uint16_t data = ReadMicro();
         data &= 0x0F; // check 4 LSBs
 
         MaxHitsPerEvent result = MaxHitsPerEvent::INVALID;
-        if( data <= static_cast<uint16_t>( MaxHitsPerEvent::NO_LIMIT ) )
-        {
+        if (data <= static_cast<uint16_t>( MaxHitsPerEvent::NO_LIMIT )) {
             result = static_cast<MaxHitsPerEvent>( data );
         }
         return result;
     }
 
-    void V1190B::WriteEnableErrMark( bool status )
-    {
-        if( status ) WriteMicro( Opcode( Command::EN_ERROR_MARK ) );
-        else         WriteMicro( Opcode( Command::DIS_ERROR_MARK ) );
+    void V1190B::WriteEnableErrMark(bool status) {
+        if (status) WriteMicro(Opcode(Command::EN_ERROR_MARK));
+        else WriteMicro(Opcode(Command::DIS_ERROR_MARK));
     }
 
-    void V1190B::WriteEnableBypass( bool status )
-    {
-        if( status ) WriteMicro( Opcode( Command::EN_ERROR_BYPASS ) );
-        else         WriteMicro( Opcode( Command::DIS_ERROR_BYPASS ) );
+    void V1190B::WriteEnableBypass(bool status) {
+        if (status) WriteMicro(Opcode(Command::EN_ERROR_BYPASS));
+        else WriteMicro(Opcode(Command::DIS_ERROR_BYPASS));
     }
 
-    void V1190B::WriteErrorPattern( uint16_t pattern )
-    {
-        WriteMicro( Opcode( Command::SET_ERROR_TYPES ) );
-        WriteMicro( pattern & IError_t::ALL );
+    void V1190B::WriteErrorPattern(uint16_t pattern) {
+        WriteMicro(Opcode(Command::SET_ERROR_TYPES));
+        WriteMicro(pattern & IError_t::ALL);
     }
 
-    uint16_t V1190B::ReadErrorPattern()
-    {
-        WriteMicro( Opcode( Command::READ_ERROR_TYPES ) );
+    uint16_t V1190B::ReadErrorPattern() {
+        WriteMicro(Opcode(Command::READ_ERROR_TYPES));
         return ReadMicro() & IError_t::ALL; // return only 11 LSBs
     }
 
-    void V1190B::WriteEffSizeFIFO( V1190B::FIFOSize size )
-    {
-        WriteMicro( Opcode( Command::SET_FIFO_SIZE ) );
-        WriteMicro( static_cast<uint16_t>( size ) );
+    void V1190B::WriteEffSizeFIFO(V1190B::FIFOSize size) {
+        WriteMicro(Opcode(Command::SET_FIFO_SIZE));
+        WriteMicro(static_cast<uint16_t>( size ));
     }
 
-    uint16_t V1190B::ReadEffSizeFIFO()
-    {
-        WriteMicro( Opcode( Command::READ_FIFO_SIZE ) );
+    uint16_t V1190B::ReadEffSizeFIFO() {
+        WriteMicro(Opcode(Command::READ_FIFO_SIZE));
         uint16_t data = ReadMicro() & 0x0007; // save only 3 LSB
         return (2 << data); // starts with 2 (if data == 0) ---  not 1;
     }
 
     // MISC
-    uint32_t V1190B::ReadTDC_ID( V1190B::TDC tdc )
-    {
-        switch( tdc )
-        {
-            case( TDC::TDC0 ) :
-                WriteMicro( Opcode( Command::READ_TDC_ID, 0x00 ) );
+    uint32_t V1190B::ReadTDC_ID(V1190B::TDC tdc) {
+        switch (tdc) {
+            case (TDC::TDC0) :
+                WriteMicro(Opcode(Command::READ_TDC_ID, 0x00));
                 break;
-            case( TDC::TDC1 ) :
-                WriteMicro( Opcode( Command::READ_TDC_ID, 0x01 ) );
+            case (TDC::TDC1) :
+                WriteMicro(Opcode(Command::READ_TDC_ID, 0x01));
                 break;
         }
         // NOTE : there is a typo in the doc
@@ -493,103 +463,125 @@ namespace vmeplus {
         return ((msb << 16U) & 0xffff0000) | (lsb & 0x0000ffff);
     }
 
-    uint16_t V1190B::ReadMicroFWRev()
-    {
-        WriteMicro( Opcode( Command::READ_MICRO_REV ) );
-        return (ReadMicro() & 0x00FF );
+    uint16_t V1190B::ReadMicroFWRev() {
+        WriteMicro(Opcode(Command::READ_MICRO_REV));
+        return (ReadMicro() & 0x00FF);
     }
 
-    void V1190B::WriteResetLoops()
-    {
-        WriteMicro( Opcode( Command::RESET_DLL_PLL ) );
+    void V1190B::WriteResetLoops() {
+        WriteMicro(Opcode(Command::RESET_DLL_PLL));
     }
 
     //****** READING ROM + ******
-    uint16_t V1190B::ReadVersion()
-    {
+    uint16_t V1190B::ReadVersion() {
         //Data seems to be a 1-byte long
-        fVersion = ReadRegister16( V1190B_VERSION, 0x00FFU );
+        fVersion = ReadRegister16(V1190B_VERSION, 0x00FFU);
         return fVersion;
     }
 
-    uint32_t V1190B::ReadBoardID()
-    {
+    uint32_t V1190B::ReadBoardID() {
         //Data seems to be a 1-byte long
-        uint32_t msb = ReadRegister16( V1190B_BOARD_ID_MSB, 0x00FF );
-        uint32_t middle = ReadRegister16( V1190B_BOARD_ID, 0x00FF );
-        uint32_t lsb = ReadRegister16( V1190B_BOARD_ID_LSB, 0x00FF );
+        uint32_t msb = ReadRegister16(V1190B_BOARD_ID_MSB, 0x00FF);
+        uint32_t middle = ReadRegister16(V1190B_BOARD_ID, 0x00FF);
+        uint32_t lsb = ReadRegister16(V1190B_BOARD_ID_LSB, 0x00FF);
 
         fBoardID = (lsb & 0xFFUL) | ((middle & 0xFFUL) << 8U) | ((msb & 0xFFUL) << 16);
 
         return fBoardID;
     }
 
-    uint32_t V1190B::ReadOUI()
-    {
+    uint32_t V1190B::ReadOUI() {
         //Data seems to be a 1-byte long
-        uint32_t msb    = ReadRegister16( V1190B_OUI_MSB, 0x00FFU );
-        uint32_t middle = ReadRegister16( V1190B_OUI, 0x00FFU );
-        uint32_t lsb    = ReadRegister16( V1190B_OUI_LSB, 0x00FFU );
+        uint32_t msb = ReadRegister16(V1190B_OUI_MSB, 0x00FFU);
+        uint32_t middle = ReadRegister16(V1190B_OUI, 0x00FFU);
+        uint32_t lsb = ReadRegister16(V1190B_OUI_LSB, 0x00FFU);
         fOUI = (lsb) | (middle << 8U) | (msb << 16U);
         return fOUI;
-    } 
+    }
 
-    uint32_t V1190B::ReadRevision()
-    {
+    uint32_t V1190B::ReadRevision() {
         //Data seems to be a 1-byte long
-        uint32_t msb = ReadRegister16( V1190B_REVISION_MSB, 0x00FF );
-        uint32_t mid1 = ReadRegister16( V1190B_REVISION_M1, 0x00FF );
-        uint32_t mid2 = ReadRegister16( V1190B_REVISION_M2, 0x00FF );
-        uint32_t lsb = ReadRegister16( V1190B_REVISION_LSB, 0x00FF );
+        uint32_t msb = ReadRegister16(V1190B_REVISION_MSB, 0x00FF);
+        uint32_t mid1 = ReadRegister16(V1190B_REVISION_M1, 0x00FF);
+        uint32_t mid2 = ReadRegister16(V1190B_REVISION_M2, 0x00FF);
+        uint32_t lsb = ReadRegister16(V1190B_REVISION_LSB, 0x00FF);
         fRevision = (lsb) | (mid2 << 8U) | (mid1 << 12U) | (msb << 16U);
         return fRevision;
     }
 
-    uint16_t V1190B::ReadSerialNumber()
-    {
-        uint16_t msb = ReadRegister16( V1190B_SERIAL_MSB, 0x00FFU );
-        uint16_t lsb = ReadRegister16( V1190B_SERIAL_LSB, 0x00FFU );
-        fSerial = (lsb & 0x00FFU) | (msb << 8U); 
-        return fSerial; 
+    uint16_t V1190B::ReadSerialNumber() {
+        uint16_t msb = ReadRegister16(V1190B_SERIAL_MSB, 0x00FFU);
+        uint16_t lsb = ReadRegister16(V1190B_SERIAL_LSB, 0x00FFU);
+        fSerial = (lsb & 0x00FFU) | (msb << 8U);
+        return fSerial;
     }
 
-    uint16_t V1190B::ReadFirmRevNumber()
-    {
-        uint16_t data = ReadRegister16( V1190B_FIRMWARE_REVISION );
-        fFirmwareRevision =  std::to_string( (data & 0x00F0U) >> 4U ) + "." +
-                             std::to_string( (data & 0x000FU) );
+    uint16_t V1190B::ReadFirmRevNumber() {
+        uint16_t data = ReadRegister16(V1190B_FIRMWARE_REVISION);
+        fFirmwareRevision = std::to_string((data & 0x00F0U) >> 4U) + "." +
+                            std::to_string((data & 0x000FU));
         fFirmware = fFirmwareRevision;
         return data;
     }
+
     //****** READING ROM - ******
-    void V1190B::WriteDummy32( uint32_t data )
-    {
-        WriteRegister32( V1190B_DUMMY_32, data );
+    void V1190B::WriteDummy32(uint32_t data) {
+        WriteRegister32(V1190B_DUMMY_32, data);
     }
 
-    uint32_t V1190B::ReadDummy32()
-    {
-        return ReadRegister32( V1190B_DUMMY_32 );
+    uint32_t V1190B::ReadDummy32() {
+        return ReadRegister32(V1190B_DUMMY_32);
     }
 
-    void V1190B::WriteDummy16( uint16_t data )
-    {
-        WriteRegister16( V1190B_DUMMY_16, data );
+    void V1190B::WriteDummy16(uint16_t data) {
+        WriteRegister16(V1190B_DUMMY_16, data);
     }
 
-    uint16_t V1190B::ReadDummy16()
-    {
-        return ReadRegister16( V1190B_DUMMY_16 );
+    uint16_t V1190B::ReadDummy16() {
+        return ReadRegister16(V1190B_DUMMY_16);
     }
 
-    void V1190B::WritePOut( V1190B::ProgOUT pout )
-    {
-        WriteRegister16( V1190B_OUT_PROG_CONTROL, static_cast<uint16_t>( pout ), 0x0003 );
+    void V1190B::WritePOut(V1190B::ProgOUT pout) {
+        WriteRegister16(V1190B_OUT_PROG_CONTROL, static_cast<uint16_t>( pout ), 0x0003);
     }
 
-    V1190B::ProgOUT V1190B::ReadPOut()
-    {
-        uint16_t data = ReadRegister16( V1190B_OUT_PROG_CONTROL, 0x0003 );
+    V1190B::ProgOUT V1190B::ReadPOut() {
+        uint16_t data = ReadRegister16(V1190B_OUT_PROG_CONTROL, 0x0003);
         return static_cast<ProgOUT>( data );
+    }
+
+    //NLI
+
+    void V1190B::SetBit16(uint32_t address, uint16_t bit) {
+        WriteRegister16(address, ReadRegister32(address) | (1 << bit));
+    }
+
+    void V1190B::ClearBit16(uint32_t address, uint16_t bit) {
+        WriteRegister16(address, ReadRegister32(address) & ~(1 << bit));
+    }
+
+    void V1190B::EnableReadoutSRAM(bool status) {
+        if (status)
+            SetBit16(V1190B_CONTROL_REGISTER, V1190B_READ_COMPENSATION_SRAM_ENABLE);
+        else
+            ClearBit16(V1190B_CONTROL_REGISTER, V1190B_READ_COMPENSATION_SRAM_ENABLE);
+    }
+
+    void V1190B::CompensationSRAM(TrLeadLSB lsb, uint8_t channel, std::vector<uint16_t> &vector) {
+        if (ReadRegister32(V1190B_CONTROL_REGISTER, V1190B_READ_COMPENSATION_SRAM_ENABLE) == V1190B_READ_COMPENSATION_SRAM_ENABLE) {
+            switch (lsb) {
+                case TrLeadLSB::ps100:
+                    WriteRegister16(V1190B_COMPENSATION_SRAM_PAGE, channel);
+                    for (int i = 0; i < 256; i++)
+                        vector.push_back(ReadRegister16(V1190B_COMPENSATION_SRAM + static_cast<uint32_t>(i)));
+                case TrLeadLSB::ps200:
+                    WriteRegister16(V1190B_COMPENSATION_SRAM_PAGE, channel);
+                    for (int i = 0; i < 128; i++)
+                        vector.push_back(ReadRegister16(V1190B_COMPENSATION_SRAM + static_cast<uint32_t>(i)));
+                default:
+                    PrintMessage(Message_t::WARNING, "INL conpensation not needed for the given values");
+            }
+        } else
+            PrintMessage(Message_t::ERROR, "Read compensation SRAM bit disable");
     }
 }
