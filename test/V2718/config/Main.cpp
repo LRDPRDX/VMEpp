30c0394c64820d63b3072eb6329045d0ffad0ef5
LRDPRDX <paradox1859@gmail.com>
2021-09-18 01:26:24 +0700

    Add serialize to configs

--- test/V2718/config/Main.cpp
+++ test/V2718/config/Main.cpp
@@ -13,22 +13,22 @@ int main()
 
     try
     {
-        controller.Open( 0, 0 );
+        //controller.Open( 0, 0 );
 
         controller.GetPulser( cvPulserA ).SetSquare( 10, 50 );
         controller.GetPulser( cvPulserA ).SetNPulses( 88 );
         controller.GetPulser( cvPulserA ).SetStartSource( cvManualSW );
         controller.GetPulser( cvPulserA ).SetStopSource( cvManualSW );
-        controller.GetPulser( cvPulserA ).Write();
+        //controller.GetPulser( cvPulserA ).Write();
 
         controller.GetPulser( cvPulserB ).SetSquare( 10000, 20 );
         controller.GetPulser( cvPulserB ).SetNPulses( 200 );
         controller.GetPulser( cvPulserB ).SetStartSource( cvManualSW );
         controller.GetPulser( cvPulserB ).SetStopSource( cvManualSW );
-        controller.GetPulser( cvPulserB ).Write();
+        //controller.GetPulser( cvPulserB ).Write();
 
         UConfig<V2718> cfg;
-        controller.ReadConfig( cfg );
+        //controller.ReadConfig( cfg );
 
         //WriteConfigToFile( cfg, "config.json" );
         WriteConfigToFile( cfg, "/usr/config.json" );
